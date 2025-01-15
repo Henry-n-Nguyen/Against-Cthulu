faf6703be8f58c2d40a618c9f4f7c3df58aed30f
Henry-n-Nguyen <70119608+Henry-n-Nguyen@users.noreply.github.com>
2025-01-15 21:50:03 +0700

    tmp

--- Assets/_Game/Scripts/CutSceneTool/CutScenePlayableAsset/DialogueMono.cs
+++ Assets/_Game/Scripts/CutSceneTool/CutScenePlayableAsset/DialogueMono.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class DialogueMono : PlayableAsset
 {
